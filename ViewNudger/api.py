8472c46310b162e8962ddee285394e121a0150a9
chrisdevito <chrisdevito@chribis.com>
2016-05-15 21:07:34 -0400

    Testing doc syntax.

--- ViewNudger/api.py
+++ ViewNudger/api.py
@@ -26,8 +26,9 @@ def parseArgs(transformName,
     :param view: Optional desired M3dView.
     :type view: OpenMaya.M3dView
 
-    :raises: ``RuntimeError`` If transformName isn't a transform or doesn't exist.
-    :raises: ``RuntimeError`` If view set is not a view.
+    :raises:
+        ``RuntimeError`` If transformName isn't a transform or doesn't exist.
+        ``RuntimeError`` If view set is not a view.
 
     :return:
         None
